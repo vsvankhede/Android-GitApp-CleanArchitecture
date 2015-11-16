dd04fa9e5c193b49ea5fd7d8393d6dc0d8fc747f
vsvankhede <vijay.s.vankhede@gmail.com>
2015-11-17 00:42:37 +0530

    Filemanger implementation for cache.

---
+++ data/src/main/java/com/vstechlab/demo/data/cache/FileManager.java
@@ -0,0 +1,11 @@
+package com.vstechlab.demo.data.cache;
+
+import javax.inject.Singleton;
+
+/**
+ * Helper class to do operation on regular files/directories.
+ */
+@Singleton
+public class FileManager {
+
+}
